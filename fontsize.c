0f8308211eb6df929fd911771b741ef30383565c
distjr_ <60539875+ruufly@users.noreply.github.com>
2025-01-30 22:51:18 +0800

    Create fontsize.c

---
+++ fontsize.c
@@ -0,0 +1,20 @@
+#include <windows.h>
+
+void setFontSizeMode(int n)
+{
+	CONSOLE_FONT_INFOEX cfi;
+	cfi.cbSize = sizeof(cfi);
+	cfi.nFont = 0;
+	cfi.dwFontSize.X = 0;
+	cfi.dwFontSize.Y = n;
+	cfi.FontFamily = FF_DONTCARE;
+	cfi.FontWeight = FW_NORMAL;
+	wcscpy(cfi.FaceName, L"Consolas");
+	SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
+}
+
+int main(int argc, char* argv[])
+{
+    setFontSizeMode(atoi(argv[1]));
+    return 0;
+}
