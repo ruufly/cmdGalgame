e6cf0d06696008e545333523751e42811308b50f
distjr_ <60539875+ruufly@users.noreply.github.com>
2025-02-07 19:09:40 +0800

    Add files via upload

---
+++ plugin/test/__init__.py
@@ -0,0 +1,15 @@
+import time
+
+# api[0]: Basic Classes and Functions
+# api[1]: Variables
+# api[2]: Styles
+api = ...
+
+def init(*args, **kwargs):
+    print("    Welcome! @test 1.0.0")
+    time.sleep(0.2)
+
+def loop(*args, **kwargs):
+    ...
+
+styles = {}
